ecbc7ada24de80e5feb8eac588916213e8478d10
Manz <manoj5115@gmail.com>
2020-05-17 22:21:27 +0530

    Integrated leap year functionality in main code and made sure all test criteria are met

--- src/main/java/com/bleedev/leapyear/LeapYearConsoleAppApplication.java
+++ src/main/java/com/bleedev/leapyear/LeapYearConsoleAppApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class LeapYearConsoleAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -14,7 +17,8 @@ public class LeapYearConsoleAppApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Starting LeapYear Application...");
-
+		boolean result = checkLeapYear("2100");
+		log.info("Leap year result: " + result);
 		System.out.println("LeapYear Application stopped");
 	}
 
